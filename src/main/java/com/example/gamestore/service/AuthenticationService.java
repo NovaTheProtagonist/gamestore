bfca48ede7bcbc45d0a8eb428aa6290cb5052e76
NovaTheProtagonist <marosoliver2003@gmail.com>
2024-05-15 20:23:59 +0200

    Implemented Logout Service Feature

---
+++ src/main/java/com/example/gamestore/service/AuthenticationService.java
@@ -0,0 +1,33 @@
+package com.example.gamestore.service;
+
+import com.example.gamestore.entity.User;
+import com.example.gamestore.model.request.LoginRequest;
+import com.example.gamestore.model.request.RegisterRequest;
+import com.example.gamestore.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
+@Setter
+public class AuthenticationService {
+    private UserRepository userRepository;
+
+    public void handleRegister(RegisterRequest registerRequest) {
+
+    }
+
+    public void handleLogin(LoginRequest loginRequest) {
+
+    }
+
+    public void handleLogout(Long userId) {
+        setUserOffline(userId);
+    }
+
+    @Transactional
+    private void setUserOffline(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow();
+        user.setStatus(User.UserStatus.OFFLINE);
+    }
+}
