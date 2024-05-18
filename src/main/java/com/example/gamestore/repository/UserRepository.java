b4ad0c4d13b3b684a76690c1141e91b058897c0e
NovaTheProtagonist <marosoliver2003@gmail.com>
2024-05-18 12:07:40 +0200

    Changed Query Type To Optional

--- src/main/java/com/example/gamestore/repository/UserRepository.java
+++ src/main/java/com/example/gamestore/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.example.gamestore.repository;
 
+import java.util.Optional;
+
 import com.example.gamestore.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserByUsernameAndPassword(String username, String password);
+    Optional<User> findUserByUsernameAndPassword(String username, String password);
 }
