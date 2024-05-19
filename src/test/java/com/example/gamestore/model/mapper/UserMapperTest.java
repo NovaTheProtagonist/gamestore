b0111bed5ff2b3c731e65c3220e0b1b3c3fcd388
NovaTheProtagonist <marosoliver2003@gmail.com>
2024-05-19 13:04:28 +0200

    Created Test For UserMapper

---
+++ src/test/java/com/example/gamestore/model/mapper/UserMapperTest.java
@@ -0,0 +1,27 @@
+package com.example.gamestore.model.mapper;
+
+import com.example.gamestore.entity.User;
+import com.example.gamestore.model.request.RegisterRequest;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserMapperTest {
+
+    @Test
+    void mapRegisterRequestToUser() {
+        //given
+        RegisterRequest registerRequest = RegisterRequest.builder().username("asd").password("123").build();
+        User expectedUser = new User();
+        expectedUser.setRole(User.Role.USER);
+        expectedUser.setGameLibrary(new ArrayList<>());
+        expectedUser.setUsername(registerRequest.getUsername());
+        expectedUser.setPassword(registerRequest.getPassword());
+        //when
+        User actualUser = UserMapper.mapRegisterRequestToUser(registerRequest);
+        //then
+        assertEquals(expectedUser, actualUser);
+    }
+}
