9b04431c1ad1a6f449e8ed50e911564df8167099
NovaTheProtagonist <marosoliver2003@gmail.com>
2024-05-20 18:26:06 +0200

    Created Initial H2 Config

---
+++ src/main/java/com/example/gamestore/h2config/InitGames.java
@@ -0,0 +1,32 @@
+package com.example.gamestore.h2config;
+
+import java.util.List;
+
+import com.example.gamestore.entity.Game;
+import com.example.gamestore.repository.GameRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitGames implements CommandLineRunner {
+
+    private GameRepository gameRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        initGames();
+    }
+
+    @Transactional
+    private void initGames() {
+        Game game = new Game();
+        game.setName("gametest");
+        game.setPrice(10f);
+        game.setGameCategories(List.of(Game.GameCategory.ACTION));
+        game.setThumbnail("image1");
+        gameRepository.save(game);
+    }
+}
---
+++ src/main/java/com/example/gamestore/h2config/InitUsers.java
@@ -0,0 +1,38 @@
+package com.example.gamestore.h2config;
+
+import com.example.gamestore.entity.User;
+import com.example.gamestore.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitUsers implements CommandLineRunner {
+    private UserRepository userRepository;
+
+    @Transactional
+    private void initTestUser() {
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("test");
+        user.setRole(User.Role.USER);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    private void initTestAdmin() {
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("admin");
+        user.setRole(User.Role.ADMIN);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        initTestUser();
+        initTestAdmin();
+    }
+}
